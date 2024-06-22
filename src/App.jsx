4432ad1300631207070f3e0a135049b40abb67fa
billywcollins <billy.collins86@gmail.com>
2024-06-22 06:17:51 -0500

    footer updates

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,51 @@ import monogram from './assets/Monogram.png'
 import './App.css'
 import FeatherIcon from 'feather-icons-react'
 import { motion } from "framer-motion"
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
       <div className='container'>
-        <motion.div 
-          className='logo-div'
-          initial={{ opacity: 0, scale: 0.9 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{
-            duration: 0.7,
-            ease: [.25,.1,.25,1]
-          }}>
-            <img className="logo-image" src={logo} />
+        <div className="logo-conainer">
+          <motion.div
+            className='circle-container'
+            initial={{ opacity: 0, scale: 0, rotate: 0 }}
+            animate={{ opacity: 1, scale: 1, rotate: 1080 }}
+            transition={{
+              duration: 1.5,
+              type: "spring"
+            }}
+          >
+            <img src={monogram} className='circles'/>
           </motion.div>
+
+          <motion.div
+            className='phenyx'
+            initial={{ opacity: 0, x:500 }}
+            animate={{ opacity: 1, x:"-10%" }}
+            transition={{
+              duration:1.5,
+              type:"spring"
+            }}
+          >
+            <h1>Phenyx</h1>
+          </motion.div>
+
+          <motion.div
+            className='studios'
+            initial={{ opacity: 0, x:-500 }}
+            animate={{ opacity: 1, x:"10%" }}
+            transition={{
+              duration:1.5,
+              type:"spring"
+            }}
+          >
+            <h1>Studios</h1>
+          </motion.div>
+        </div>
+        
         <motion.div
           className='coming-soon-div'
           initial={{ opacity: 0 }}
@@ -31,6 +60,19 @@ function App() {
           }}>
           <span className='coming-soon'>Coming Soon</span>
         </motion.div>
+
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{
+            duration: 1.0,
+            delay: 1.0,
+            ease: "linear"
+          }}
+        >
+          <Footer />
+        </motion.div>
+        
       </div>
     </>
   )
---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <footer>
+        <ul className="footer-list">
+        <li className="footer-item">&copy; 2024 Phenyx AI Studios</li>
+        <li className="footer-item">Privacy</li>
+        <li className="footer-item">Terms</li>
+        <li className="footer-item">Work with Us</li>
+        </ul>
+    </footer>
+  )
+}
+
+export default Footer
